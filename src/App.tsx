dac6620febb10363aa6c83cc5eb0cd5e9e2db432
idimitrisc47 <dimitrisc47@gmail.com>
2024-06-20 11:06:14 -0500

    Criando Estrutura Base

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import styles from './App.module.css';
+import poweredImage from './assets/powered.png';
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className={styles.main}>
+      <header>
+        <div className={styles.headerContainer}>
+          <img src={poweredImage} width={150} alt="powered by b7web" />
+        </div>
       </header>
+      <div className={styles.container}>
+        <div className={styles.leftSide}>
+          ...
+        </div>
+        <div className={styles.rightSide}>
+          ...
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
