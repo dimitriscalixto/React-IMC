682a6160a03ed1ea8c84b0752f29b5305c9e3306
idimitrisc47 <dimitrisc47@gmail.com>
2024-06-20 11:16:57 -0500

    Criando o lefside 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,16 @@
+import { useState } from 'react';
 import styles from './App.module.css';
 import poweredImage from './assets/powered.png';
 const App = () => {
+  const [heightField, setHeightField] = useState<number>(0);
+  const [weightField, setWeightField] = useState<number>(0);
+  const handleCalculateButton = () => {
+    if(heightField && weightField){
+
+    } else {
+      alert('Digite todos os campos');
+    }
+  }
   return (
     <div className={styles.main}>
       <header>
@@ -10,8 +20,24 @@ const App = () => {
       </header>
       <div className={styles.container}>
         <div className={styles.leftSide}>
-          ...
+          <h1>Calcule o seu IMC.</h1>
+          <p>IMC é a sigla para Índice de Massa corporea, parâmetro adotado pela Organização Mundo de Saúde para calcular o peso ideal de cada pessoa.</p>
+        
+          <input
+          type="number"
+          placeholder='Digite sua altura. Ex: 1.5 (em metros)'
+          value={heightField > 0 ? heightField: ''}
+          onChange={(e) => setHeightField(parseFloat(e.target.value))}
+          />
+          <input
+          type="number"
+          placeholder='Digite o seu peso. Ex: 75.3 (em kg)'
+          value={weightField > 0 ? weightField: ''}
+          onChange={(e) => setWeightField(parseFloat(e.target.value))}
+          />
+          <button onClick={handleCalculateButton}>Calcular</button> 
         </div>
+        
         <div className={styles.rightSide}>
           ...
         </div>
